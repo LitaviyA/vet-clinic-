1d17623482d55d7d5671661b01082699a1984be4
Admin <litaviy@gmail.com>
2024-01-10 10:39:34 +0200

    REQ-03: Add question

--- src/main/java/com/magicvet/componet/ApplicationRunner.java
+++ src/main/java/com/magicvet/componet/ApplicationRunner.java
@@ -1,5 +1,6 @@
 package main.java.com.magicvet.componet;
 
+import main.java.com.magicvet.Main;
 import main.java.com.magicvet.model.Client;
 import main.java.com.magicvet.model.Pet;
 import main.java.com.magicvet.service.ClientService;
@@ -14,11 +15,17 @@ public class ApplicationRunner {
             Client client = clientService.registerNewClient();
 
             if(client != null){
-                System.out.println("Adding a new pet.");
-                Pet pet = petService.registerNewPet();
-                client.setPet(pet);
-                pet.setOwnerName(client.getFirstName() + " " + client.getLastName());
-                System.out.println("Pet has been added.");
+                System.out.print("Do you want to add pet now? yes/no: ");
+                String answerClient = Main.SCANNER.nextLine();
+                if(answerClient.equals("yes")){
+                    System.out.println("Adding a new pet.");
+                    Pet pet = petService.registerNewPet();
+                    client.setPet(pet);
+                    pet.setOwnerName(client.getFirstName() + " " + client.getLastName());
+                    System.out.println("Pet has been added.");
+                } else if (!answerClient.equals("no")) {
+                    System.out.println("Select yes or no.");
+                }
             }
         }
     }
